0500d4db8990204b7b59ee1a136cf2e67256cbd7
ilya Kupriyanov <ilya-kyp@yandex.ru>
2025-05-28 19:59:58 +0300

    Lesson 6

--- src/SaveStudentOrder.java
+++ src/SaveStudentOrder.java
@@ -1,10 +1,18 @@
 
 public class SaveStudentOrder {
-    
+
     public static void main(String[] args) {
-        saveStudentOrder();
+        StudentOrder so = new StudentOrder();
+        so.hFirstName = "Алексей";
+        so.hLastName = "Петров";
+        so.wFirstName = "Галина";
+        so.wLastName = "Петрова";
+        long ans = saveStudentOrder(so);
+        System.out.println(ans);
     }
-    static void saveStudentOrder(){
-        System.out.println("saveStudentOrder");
+    static long saveStudentOrder(StudentOrder studentOrder){
+        long answer = 199;
+        System.out.println("hLastName = " + studentOrder.hLastName);
+        return answer;
     }
 }
