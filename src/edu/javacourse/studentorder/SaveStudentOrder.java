f0927d67e0633026eb2ccae54560b0398617170b
ilya Kupriyanov <ilya-kyp@yandex.ru>
2025-05-28 22:00:40 +0300

    Lesson 11

--- src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -7,16 +7,16 @@ public class SaveStudentOrder {
 
     public static void main(String[] args) {
         StudentOrder so = new StudentOrder();
-        so.hFirstName = "Алексей";
-        so.hLastName = "Петров";
-        so.wFirstName = "Галина";
-        so.wLastName = "Петрова";
+        so.sethFirstName("Алексей");
+        so.sethLastName("Петров");
+        so.setwFirstName("Галина");
+        so.setwLastName("Петрова");
         long ans = saveStudentOrder(so);
         System.out.println(ans);
     }
     static long saveStudentOrder(StudentOrder studentOrder){
         long answer = 199;
-        System.out.println("hLastName = " + studentOrder.hLastName);
+        System.out.println("hLastName = " + studentOrder.gethLastName());
         return answer;
     }
 
