4470280be9628fb489ffe5b8304166b6d7c308d2
ilya Kupriyanov <ilya-kyp@yandex.ru>
2025-05-28 20:29:51 +0300

    Lesson 8

--- src/StudentOrderValidator.java
+++ src/StudentOrderValidator.java
@@ -4,14 +4,20 @@ public class StudentOrderValidator {
     }
 
     private static void checkAll() {
-        StudentOrder so = readStudentOrder();
+        while (true) {
+            StudentOrder so = readStudentOrder();
 
-        AnswerCityRegister cityAnswer = checkCityRegister(so);
-        AnswerWedding wedAnswer = checkWedding(so);
-        AnswerChildren childAnswer = checkChildren(so);
-        AnswerStudent studentAnswer = checkStudent(so);
+            if (so == null) return;
 
-        sendMail(so);
+            AnswerCityRegister cityAnswer = checkCityRegister(so);
+            if (!cityAnswer.success) continue;
+            AnswerWedding wedAnswer = checkWedding(so);
+            AnswerChildren childAnswer = checkChildren(so);
+            AnswerStudent studentAnswer = checkStudent(so);
+
+            sendMail(so);
+        }
+
     }
 
     static void sendMail(StudentOrder so) {
@@ -21,22 +27,24 @@ public class StudentOrderValidator {
         return new StudentOrder();
     }
 
-    static AnswerCityRegister checkCityRegister(StudentOrder so){
+    static AnswerCityRegister checkCityRegister(StudentOrder so) {
         System.out.println("checkCityRegister");
-        return new AnswerCityRegister();
+        AnswerCityRegister result = new AnswerCityRegister();
+        result.success = true;
+        return result;
     }
 
-    static AnswerWedding checkWedding(StudentOrder so){
+    static AnswerWedding checkWedding(StudentOrder so) {
         System.out.println("checkWedding");
         return new AnswerWedding();
     }
 
-    static AnswerChildren checkChildren(StudentOrder so){
+    static AnswerChildren checkChildren(StudentOrder so) {
         System.out.println("checkChildren");
         return new AnswerChildren();
     }
 
-    static AnswerStudent checkStudent(StudentOrder so){
+    static AnswerStudent checkStudent(StudentOrder so) {
         System.out.println("checkStudent");
         return new AnswerStudent();
     }
