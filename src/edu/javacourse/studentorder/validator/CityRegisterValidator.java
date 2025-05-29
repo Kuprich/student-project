7dcc1d5b8558b89290262cf8b10b3b1c7372df27
ilya Kupriyanov <ilya-kyp@yandex.ru>
2025-05-29 09:44:56 +0300

    Lesson 20

--- src/edu/javacourse/studentorder/validator/CityRegisterValidator.java
+++ src/edu/javacourse/studentorder/validator/CityRegisterValidator.java
@@ -5,12 +5,17 @@ import edu.javacourse.studentorder.domain.StudentOrder;
 
 public class CityRegisterValidator {
 
-    public String hostName;
-    public String login;
-    public String password;
+    private CityRegisterChecker personChecker;
+
+    public CityRegisterValidator() {
+        personChecker = new RealCityRegisterChecker();
+    }
 
     public AnswerCityRegister checkCityRegister(StudentOrder so) {
-        System.out.println("checkCityRegister, hostname: " + hostName + " login: " + login + " password: " + password);
+        personChecker.checkPerson(so.getHusband());
+        personChecker.checkPerson(so.getWife());
+        personChecker.checkPerson(so.getChild());
+
         AnswerCityRegister result = new AnswerCityRegister();
         result.success = true;
         return result;
