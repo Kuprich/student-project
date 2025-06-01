2fc25d39104793908dde4fdf5a46819bb39bbb75
ilya Kupriyanov <ilya-kyp@yandex.ru>
2025-06-01 20:50:55 +0300

    Lesson 66

---
+++ src/test/java/edu/javacourse/studentorder/dao/DictionaryDaoImplTest.java
@@ -0,0 +1,14 @@
+package edu.javacourse.studentorder.dao;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+
+public class DictionaryDaoImplTest {
+
+    @Test
+    public void testExample(){
+
+    }
+
+}
