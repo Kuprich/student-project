421f74745d8abaae6b570146daea7354ad5d7fea
ilya Kupriyanov <ilya-kyp@yandex.ru>
2025-06-03 13:26:06 +0300

    Lesson 86

--- ext-systems/city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
+++ ext-systems/city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
@@ -10,7 +10,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class PersonCheckDao {
-    private static final String SQL_REQUEST = "";
+    private static final String SQL_REQUEST = "select * " +
+            "from cr_address_person cr_ap " +
+            "inner join cr_address cr_a on cr_a.address_id = cr_ap.address_id " +
+            "inner join cr_person cr_p on cr_p.person_id = cr_ap.person_id " +
+            "where  cr_p.sur_name ilike 'иванов' " +
+            "and cr_p.given_name ilike 'иван' " +
+            "and cr_p.patronymic ilike 'иванович' " +
+            "and cr_p.date_of_birth = '1980-05-15' " +
+            "and cr_a.street_code = 1 " +
+            "and cr_a.building ilike '10' " +
+            "and cr_a.\"extension\" ilike 'А' " +
+            "and cr_a.apartment ilike '15'";
 
     public PersonResponse checkPerson(PersonRequest request) throws PersonCheckException {
         PersonResponse response = new PersonResponse();
