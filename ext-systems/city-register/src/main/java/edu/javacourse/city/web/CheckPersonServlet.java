ff0dc397756a98ae01a29625250e8a79be7448fa
ilya Kupriyanov <ilya-kyp@yandex.ru>
2025-06-04 21:52:37 +0300

    Lesson 100

--- ext-systems/city-register/src/main/java/edu/javacourse/city/web/CheckPersonServlet.java
+++ ext-systems/city-register/src/main/java/edu/javacourse/city/web/CheckPersonServlet.java
@@ -4,6 +4,8 @@ import edu.javacourse.city.dao.PersonCheckDao;
 import edu.javacourse.city.domain.PersonRequest;
 import edu.javacourse.city.domain.PersonResponse;
 import edu.javacourse.city.extension.PersonCheckException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,6 +17,15 @@ import java.time.LocalDate;
 
 @WebServlet(name="CheckPersonServlet", urlPatterns = {"/checkPerson"})
 public class CheckPersonServlet extends HttpServlet {
+
+   private static final Logger logger = LoggerFactory.getLogger(CheckPersonServlet.class);
+   private static PersonCheckDao checker;
+
+    @Override
+    public void init() throws ServletException {
+        logger.info("CheckPersonServlet created");
+        checker = new PersonCheckDao();
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -33,7 +44,6 @@ public class CheckPersonServlet extends HttpServlet {
         preq.setApartment("15");
 
         try {
-            PersonCheckDao checker = new PersonCheckDao();
             PersonResponse presp = checker.checkPerson(preq);
 
             if (presp.isRegistered()){
