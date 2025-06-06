367b137e99a0eb477bfaf6db50e5b568fcf84737
ilya Kupriyanov <ilya-kyp@yandex.ru>
2025-06-06 18:30:23 +0300

    Lesson 106

--- ext-systems/city-register/src/main/java/edu/javacourse/city/domain/PersonRequest.java
+++ ext-systems/city-register/src/main/java/edu/javacourse/city/domain/PersonRequest.java
@@ -2,6 +2,7 @@ package edu.javacourse.city.domain;
 
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class PersonRequest {
     private String surName;
--- ext-systems/city-register/src/main/java/edu/javacourse/city/web/CheckPersonService.java
+++ ext-systems/city-register/src/main/java/edu/javacourse/city/web/CheckPersonService.java
@@ -1,22 +1,44 @@
 package edu.javacourse.city.web;
 
+import edu.javacourse.city.dao.PersonCheckDao;
+import edu.javacourse.city.dao.PoolConnectionBuilder;
 import edu.javacourse.city.domain.PersonRequest;
 import edu.javacourse.city.domain.PersonResponse;
+import edu.javacourse.city.extension.PersonCheckException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
+import javax.inject.Singleton;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("/check")
+@Singleton
 public class CheckPersonService {
+
+    private static final Logger logger = LoggerFactory.getLogger(CheckPersonService.class);
+    private PersonCheckDao checker;
+
     public CheckPersonService() {
+
+    }
+
+    @PostConstruct
+    public void init() {
+        logger.info("CheckPersonService init");
+        checker = new PersonCheckDao();
+        checker.setConnectionBuilder(new PoolConnectionBuilder());
+
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public PersonResponse checkPerson(PersonRequest request){
+    public PersonResponse checkPerson(PersonRequest request) throws PersonCheckException {
         System.out.println(request.toString());
-        return new PersonResponse();
+        PersonResponse result = checker.checkPerson(request);
+        return result;
     }
 
 }
