16afea1ba2c2101aa9623b3a62a8342ef918718c
ilya Kupriyanov <ilya-kyp@yandex.ru>
2025-06-06 21:44:54 +0300

    Lesson 108

--- student-order/src/main/java/edu/javacourse/studentorder/validator/CityRegisterValidator.java
+++ student-order/src/main/java/edu/javacourse/studentorder/validator/CityRegisterValidator.java
@@ -7,7 +7,6 @@ import edu.javacourse.studentorder.domain.StudentOrder;
 import edu.javacourse.studentorder.domain.register.AnswerCityRegisterItem;
 import edu.javacourse.studentorder.domain.register.CityRegisterResponse;
 import edu.javacourse.studentorder.exception.CityRegisterException;
-import edu.javacourse.studentorder.exception.TransportException;
 import edu.javacourse.studentorder.validator.register.CityRegisterChecker;
 import edu.javacourse.studentorder.validator.register.RealCityRegisterChecker;
 
@@ -46,10 +45,6 @@ public class CityRegisterValidator {
         } catch (CityRegisterException ex) {
             status = AnswerCityRegisterItem.CityStatus.ERROR;
             error = new AnswerCityRegisterItem.CityError(ex.getCode(), ex.getMessage());
-            ex.printStackTrace();
-        } catch (TransportException ex) {
-            status = AnswerCityRegisterItem.CityStatus.ERROR;
-            error = new AnswerCityRegisterItem.CityError(EX_T_CODE, ex.getMessage());
             ex.printStackTrace();
         }
         return new AnswerCityRegisterItem(status, person, error);
--- student-order/src/test/java/edu/javacourse/studentorder/validator/register/FakeCityRegisterChecker.java
+++ student-order/src/test/java/edu/javacourse/studentorder/validator/register/FakeCityRegisterChecker.java
@@ -5,7 +5,6 @@ import edu.javacourse.studentorder.domain.Child;
 import edu.javacourse.studentorder.domain.register.CityRegisterResponse;
 import edu.javacourse.studentorder.domain.Person;
 import edu.javacourse.studentorder.exception.CityRegisterException;
-import edu.javacourse.studentorder.exception.TransportException;
 
 public class FakeCityRegisterChecker implements CityRegisterChecker {
 
@@ -22,7 +21,7 @@ public class FakeCityRegisterChecker implements CityRegisterChecker {
     public static final String T_ERROR_2 = "2003";
 
 
-    public CityRegisterResponse checkPerson(Person person) throws CityRegisterException, TransportException {
+    public CityRegisterResponse checkPerson(Person person) throws CityRegisterException {
         CityRegisterResponse response = new CityRegisterResponse();
 
         if (person instanceof Adult adult){
