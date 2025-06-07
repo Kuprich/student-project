8b555471919d5a55351169b6f88338547945057f
ilya Kupriyanov <ilya-kyp@yandex.ru>
2025-06-07 13:25:20 +0300

    Lesson 112

--- ext-systems/register-office/src/main/java/edu/javacourse/register/manager/PersonManager.java
+++ ext-systems/register-office/src/main/java/edu/javacourse/register/manager/PersonManager.java
@@ -1,6 +1,7 @@
 package edu.javacourse.register.manager;
 
 import edu.javacourse.register.domain.Person;
+import edu.javacourse.register.utils.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
@@ -14,46 +15,71 @@ import java.util.List;
 
 public class PersonManager {
     public static void main(String[] args) {
-        SessionFactory sf = buildSessionFactory();
-        Session session = sf.openSession();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
 
-        session.getTransaction().begin();
+            session.getTransaction().begin();
 
-        Person person = new Person();
-        person.setFirstName("Иван");
-        person.setLastName("Иванов");
+            Person person = new Person();
+            person.setFirstName("Петр");
+            person.setLastName("Петров");
 
-        long id = (long) session.save(person);
-        System.out.println(id);
+            long id = (long) session.save(person);
 
-        session.getTransaction().commit();
+            System.out.println("id созданного объекта: " + id);
 
-        Person person1 = session.get(Person.class, id);
-        System.out.println(person1);
+            session.getTransaction().commit();
 
-        CriteriaBuilder cb = session.getCriteriaBuilder();
-        CriteriaQuery<Person> cq = cb.createQuery(Person.class);
-        cq.select(cq.from(Person.class));
-        List<Person> people = session.createQuery(cq).getResultList();
+            List<Person> people = session.createQuery("FROM Person", Person.class).list();
+            people.forEach(System.out::println);
 
-        people.forEach(System.out::println);
+          //  session.getTransaction().commit();
 
-        session.close();
-
-
-    }
-
-    private static SessionFactory buildSessionFactory(){
-        try{
-            StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                    .configure("hibernate.cfg.xml").build();
-
-            Metadata metadata = new MetadataSources(serviceRegistry).getMetadataBuilder().build();
-
-            return  metadata.getSessionFactoryBuilder().build();
-        } catch (Exception e) {
-            System.err.println("Initial SessionFactory creation failed: " + e);
-            throw new ExceptionInInitializerError(e);
+        } catch (Exception ex){
+            ex.printStackTrace();
         }
+        finally {
+            HibernateUtil.shutdown();
+        }
+//        SessionFactory sf = buildSessionFactory();
+//        Session session = sf.openSession();
+//
+//        session.getTransaction().begin();
+//
+//        Person person = new Person();
+//        person.setFirstName("Иван");
+//        person.setLastName("Иванов");
+//
+//        long id = (long) session.save(person);
+//        System.out.println(id);
+//
+//        session.getTransaction().commit();
+//
+//        Person person1 = session.get(Person.class, id);
+//        System.out.println(person1);
+//
+//        CriteriaBuilder cb = session.getCriteriaBuilder();
+//        CriteriaQuery<Person> cq = cb.createQuery(Person.class);
+//        cq.select(cq.from(Person.class));
+//        List<Person> people = session.createQuery(cq).getResultList();
+//
+//        people.forEach(System.out::println);
+//
+//        session.close();
+//
+
     }
+
+//    private static SessionFactory buildSessionFactory(){
+//        try{
+//            StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+//                    .configure("hibernate.cfg.xml").build();
+//
+//            Metadata metadata = new MetadataSources(serviceRegistry).getMetadataBuilder().build();
+//
+//            return  metadata.getSessionFactoryBuilder().build();
+//        } catch (Exception e) {
+//            System.err.println("Initial SessionFactory creation failed: " + e);
+//            throw new ExceptionInInitializerError(e);
+//        }
+//    }
 }
