4b9f0cf1185c9477afaaac80ad8cc055eb2dcc29
ilya Kupriyanov <ilya-kyp@yandex.ru>
2025-06-08 22:20:42 +0300

    Lesson 130

--- ext-systems/student/src/main/java/edu/javacourse/student/service/StudentService.java
+++ ext-systems/student/src/main/java/edu/javacourse/student/service/StudentService.java
@@ -1,16 +1,49 @@
 package edu.javacourse.student.service;
 
+import edu.javacourse.student.dao.StudentRepository;
+import edu.javacourse.student.domain.Student;
+import edu.javacourse.student.domain.StudentDocument;
+import edu.javacourse.student.view.StudentRequest;
+import edu.javacourse.student.view.StudentResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class StudentService {
 
+    @Autowired
+    private StudentRepository studentRepository;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(StudentService.class);
 
-    public void simpleCall() {
-        LOGGER.info("CALLED!!!");
+    public List<StudentResponse> getStudentInfo(StudentRequest request) {
+        List<Student> students = studentRepository.finStudent(request.getLastName(), request.getFirstName(),
+                request.getMiddleName(), request.getDateOfBirth(), request.getPassportSeria(),
+                request.getPassportNumber(), request.getPassportDate());
+
+        if (students.isEmpty()) return Collections.EMPTY_LIST;
+        List<StudentDocument> docs = students.get(0).getStudentDocuments();
+
+        List<StudentResponse> result = docs.stream().map(this::getResponse).collect(Collectors.toList());
+
+        return result;
+    }
+
+    private StudentResponse getResponse(StudentDocument doc) {
+        StudentResponse sr = new StudentResponse();
+        sr.setDocumentDate(doc.getDocumentDate());
+        sr.setExpiredDate(doc.getExpiresDate());
+        sr.setFacultyName(doc.getFaculty().getFacultyName());
+        sr.setUniversityName(doc.getFaculty().getUniversity().getUniversityName());
+        sr.setStudentForm(doc.getStudentForm().toString());
+
+        return sr;
     }
 
 }
