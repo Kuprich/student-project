9cb15648e514bcb4beb371fb0f0d3413a5f1424f
ilya Kupriyanov <ilya-kyp@yandex.ru>
2025-06-09 21:08:04 +0300

    Lesson 137

---
+++ ext-systems/student/src/test/java/edu/javacourse/student/service/UniversityServiceTest.java
@@ -0,0 +1,26 @@
+package edu.javacourse.student.service;
+
+
+import edu.javacourse.student.domain.University;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = {"classpath:springContext.xml"})
+public class UniversityServiceTest {
+
+    @Autowired
+    UniversityService universityService;
+
+    @Test
+    @Transactional
+    public void getUniversities_test() {
+        List<University> result = universityService.getUniversities();
+    }
+}
