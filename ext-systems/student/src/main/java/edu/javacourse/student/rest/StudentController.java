d45dcfb8256f34f43816cefd80273e998c570a96
ilya Kupriyanov <ilya-kyp@yandex.ru>
2025-06-10 19:38:02 +0300

    Lesson 148

--- ext-systems/student/src/main/java/edu/javacourse/student/rest/StudentController.java
+++ ext-systems/student/src/main/java/edu/javacourse/student/rest/StudentController.java
@@ -4,8 +4,12 @@ import edu.javacourse.student.service.StudentService;
 import edu.javacourse.student.view.StudentRequest;
 import edu.javacourse.student.view.StudentResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -23,6 +27,16 @@ public class StudentController {
     @GetMapping("/check/{id}")
     public String check(@PathVariable("id") int id, @RequestParam("value") String value) {
         return "Rest service is working. Id: " + id + " value: " + value;
+    }
+
+    @PostMapping(path = "/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String uploadPhoto(@RequestParam("comment") String comment, @RequestParam("photoFile") MultipartFile photoFile){
+        try (InputStream is = photoFile.getInputStream()) {
+            return "Comment: " + comment + ", Name: " + photoFile.getName()
+                    + " File Name: " + photoFile.getOriginalFilename() + ", Size: " + is.available();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
